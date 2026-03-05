e7e6727302b80b498f211a2ca0a7488779330a68
pfampuerogonzalez-cloud <pfampuerogonzalez@gmail.com>
2026-03-05 01:10:11 +0000

    ejemplos python incompletos

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+print("hola mundo!")
+
+#variables:
+
+nombre = "Pablo"
+edad = 27
+altura = 1.67
+es_diestro = False
+ciudad = "Rancagua"
+genero = ["hombre", "mujer", "therian"]
+
+#TIPOS DE DATOS
